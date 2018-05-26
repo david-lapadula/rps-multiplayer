4eaa2150d0980a7894f7d91adf669935c4b34e79
David L <lapadula.david@gmail.com>
2018-05-26 01:10:05 -0400

    Responsive form done

---
+++ assets/javascript/rps_logic.js
@@ -0,0 +1,23 @@
+document.ready(function () {
+    //Firebase Authentication
+    
+  // Initialize Firebase 
+  var config = {
+    apiKey: "AIzaSyAYu99F7dk_r5fh8CWkBbqrAh5YNw1SAkY",
+    authDomain: "rps-database-81187.firebaseapp.com",
+    databaseURL: "https://rps-database-81187.firebaseio.com",
+    projectId: "rps-database-81187",
+    storageBucket: "",
+    messagingSenderId: "359029810871"
+  }; 
+
+  firebase.initializeApp(config);
+
+  // Variable reference to the database
+   var rpsDatabase = firebase.database();  
+
+
+
+});  
+
+// https://github.com/angrbrd/multiplayer-rps OR https://github.com/mattbajorek/RPS-Multiplayer
